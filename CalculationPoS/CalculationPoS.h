930f21a7f0b82f6bb3db5ced76698e2f9eda38c6
Konstantin Klyatskin <klyatskin@gmail.com>
2019-10-07 14:07:05 -0400

    Initial Commit

---
+++ CalculationPoS/CalculationPoS.h
@@ -0,0 +1,19 @@
+//
+//  CalculationPoS.h
+//  CalculationPoS
+//
+//  Created by Konstantin Klyatskin on 2019-10-07.
+//  Copyright © 2019 TouchBistro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalculationPoS.
+FOUNDATION_EXPORT double CalculationPoSVersionNumber;
+
+//! Project version string for CalculationPoS.
+FOUNDATION_EXPORT const unsigned char CalculationPoSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculationPoS/PublicHeader.h>
+
+
